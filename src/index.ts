05af45147d9a18f2491d028176aa9a8bd6b24ad2
Ryan Prior <ryanprior@hey.com>
2020-11-30 09:31:26 -0600

    Adds loader override
    * src/index.ts:
      - new function `loader`
      - override to use text loader for `.template` files

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
-import { transformSync } from 'esbuild'
+import { transformSync, TransformOptions } from 'esbuild'
 import { extname, resolve } from 'path'
 import { readFileSync, existsSync } from 'fs'
 
-const getOptions = (): import('esbuild').TransformOptions => {
+function getOptions(): TransformOptions {
   const tsconfigPath = resolve('tsconfig.json')
-  const tsconfig = existsSync(tsconfigPath) 
+  const tsconfig = existsSync(tsconfigPath)
     ? JSON.parse(readFileSync(resolve('tsconfig.json'), 'utf-8'))
     : { compilerOptions: { target: 'es2018' } }
 
-   return {
-     format: 'cjs',
-     target: tsconfig.compilerOptions?.target || 'es2018',
-     sourcemap: true
-   }
+  return {
+    format: 'cjs',
+    target: tsconfig.compilerOptions?.target || 'es2018',
+    sourcemap: true
+  }
+}
+
+function loader(filename: string): string {
+  const ext = extname(filename).slice(1)
+  const overrides = {
+    'template': 'text',
+    'yml': 'text',
+    'json': 'text'
+  }
+  return overrides[ext] || ext
 }
 
 export function process(content: string, filename: string) {
   const options = getOptions()
-  
+
   const result = transformSync(content, {
-    loader: extname(filename).slice(1) as any,
+    loader: loader(filename) as any,
     sourcefile: filename,
     ...options
   })
